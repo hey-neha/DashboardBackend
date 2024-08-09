b950d22a7b7f458c6afce913e39b3e0e1cddaeca
Neha  Yadav <ny8866428@gmail.in>
2024-08-09 22:17:42 +0530

    first commit

--- Models/db.js
+++ Models/db.js
@@ -1,6 +1,6 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 // const mongo_url =
-//   "mongodb+srv://ny8866428:nWYMihJyskUNOVgh@cluster0.fza7t0y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+//   "mongodb+srv://ny8866428:JtLwxZI46RPDfPCU@cluster0.hwwpu1x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 //ny8866428
 // nWYMihJyskUNOVgh
@@ -15,7 +15,7 @@ const mongoose = require("mongoose");
 //     console.log("mongoDB Connection Error:", err);
 //   });
 
-  export const connectDB = async () => {
+/*   const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
       `${"mongodb+srv://ny8866428:nWYMihJyskUNOVgh@cluster0.fza7t0y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"}/${dashboard}`
@@ -30,3 +30,49 @@ const mongoose = require("mongoose");
 };
 
 // export default connectDB;
+ */
+
+// JtLwxZI46RPDfPCU------password
+// mongodb+srv://<username>:<password>@cluster0.hwwpu1x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+// const mongoose = require("mongoose");
+
+// const mongo_url =
+//   "mongodb+srv://ny8866428:JtLwxZI46RPDfPCU@cluster0.hwwpu1x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// const connectDB = async () => {
+//   try {
+//     const connectionInstance = await mongoose.connect(mongo_url, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log(
+//       `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+//     );
+//   } catch (error) {
+//     console.log("MONGODB connection FAILED ", error);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
+// const { MongoClient } = require('mongodb');
+
+const { MongoClient } = require("mongodb");
+
+const uri = "your_mongodb_connection_string_here";
+const client = new MongoClient(
+  "mongodb+srv://ny8866428:JtLwxZI46RPDfPCU@cluster0.hwwpu1x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+);
+
+async function connect() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("MONGODB connection FAILED", err);
+  }
+}
+
+connect();
--- index.js
+++ index.js
@@ -1,24 +1,101 @@
-const express = require("express");
+// const express = require("express");
+// const app = express();
+// require("./Models/db");
+// const AuthRouter = require("./Routers/AuthRouter");
+
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+
+// const conectDB = require("./Models/db.js");
+// conectDB();
+
+// app.get("/ping", (req, res) => {
+//   res.send("PONG");
+// });
+// app.use(bodyParser.json());
+// app.use(cors());
+// //request allow to server---------------
+
+// //router--------------------------
+// app.use("/auth", AuthRouter);
+
+// app.listen(8000, () => {
+//   console.log(`server is running on port 8000`);
+// });
+
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
 const app = express();
-require("./Models/db");
-const AuthRouter = require("./Routers/AuthRouter");
-
-const bodyParser = require("body-parser");
-const cors = require("cors");
-
-const conectDB = require("./Models/db.js");
-conectDB();
-
-app.get("/ping", (req, res) => {
-  res.send("PONG");
-});
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-//request allow to server---------------
 
-//router--------------------------
-app.use("/auth", AuthRouter);
+//nehayadav9450242664
+//4I17iqlXyCiHoRJD
+//mongodb+srv://<username>:<password>@cluster0.9ukqgml.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 
-app.listen(8000, () => {
-  console.log(`server is running on port 8000`);
+const url =
+  "mongodb+srv://manishdevloop:NIQi8aawFUjYsuSp@cluster0.mleqmvq.mongodb.net ";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(url, {});
+    console.log("MONGODB connected!!!!");
+  } catch (error) {
+    console.log("MONGODB connection FAILED ", error);
+  }
+};
+connectDB();
+
+// schema-----------------------------------
+
+const userSchema = new mongoose.Schema({
+  fullname: String,
+  email: String,
+  password: String,
+  conformedPas: String,
+});
+const User = new mongoose.model("User", userSchema);
+
+// Router--------------------
+
+app.post("/register", async (req, res) => {
+  const { fullname, email, password, conformedPas } = req.body;
+
+  try {
+    //check if the user already exists---------------
+    const user = await User.findOne({ email: email });
+
+    if (user) {
+      res.send({ message: "User already registerd!!!" });
+    } else {
+      /* const NewUser = new User({
+        fullname: fullname,
+        email: email,
+        password: password,
+        conformedPas: conformedPas,
+      }); */
+
+      //user create kr rhe hai ...mondoDB ka ---------
+      const user = await User.create({
+        fullname,
+        email,
+        password,
+        conformedPas,
+      });
+
+      //save the user to the database------
+      await user.save();
+      res.send({ message: "successfully registered!!!" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Internal Server Error" });
+  }
+});
+
+app.listen(9002, () => {
+  console.log("BE started at Port 9002");
 });
